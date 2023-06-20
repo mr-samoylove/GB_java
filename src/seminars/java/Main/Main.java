ba34d4a95d3f1a861a55bc541694a3d4a34791b3
psina <mr.samoylove@gmail.com>
2023-06-20 17:23:26 +0300

    sem5

--- src/seminars/java/Main/Main.java
+++ src/seminars/java/Main/Main.java
@@ -1,47 +1,16 @@
 package Main;
 
-import sem4.*;
+import sem5.*;
+
+import java.nio.file.Path;
 
 
 public class Main {
     public static void main(String[] args) {
+        BackUpper backUpper = new BackUpper();
+        backUpper.backUpFiles(Path.of("."));
+        backUpper.backUpFiles(Path.of("./src/seminars/java/sem4"));
 
-        // нормальный массив
-        String[][] array1 = {{"1", "22", "44", "12345"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}};
-
-        // массивы иных размеров
-        String[][] array2 = {{"1", "22", "44", "12345"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}};
-        String[][] array3 = {{"1", "22", "44"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}};
-
-        // массивы с неверными данными
-        String[][] array4 = {{"1", "22", "44", "12345"}, {"sobaka", "0", "0", "0"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}};
-        String[][] array5 = {{"1", "22", "44", "12345.54321"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}, {"0", "0", "0", "0"}};
-
-        System.out.println(ArrayHandler.sumStringArray4x4(array1));
-
-        try {
-            System.out.println(ArrayHandler.sumStringArray4x4(array2));
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            System.out.println(ArrayHandler.sumStringArray4x4(array3));
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            System.out.println(ArrayHandler.sumStringArray4x4(array4));
-        } catch (MyArrayDataException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            System.out.println(ArrayHandler.sumStringArray4x4(array5));
-        } catch (MyArrayDataException e) {
-            System.out.println(e.getMessage());
-        }
-
+        Tree.print(Path.of("./src"));
     }
 }
